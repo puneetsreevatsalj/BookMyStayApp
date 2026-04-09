403e8e674c6f3d3295a2c94997f2a2819b8bbf6e
puneetsreevatsalj <puneetsreevatsalj@gmail.com>
2026-04-09 16:47:41 +0530

    UC12: Data persistence and recovery using serialization

--- RoomInventory.java
+++ RoomInventory.java
@@ -1,48 +1,33 @@
+import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
 
-public class RoomInventory {
-    private Map<String, Integer> inventory;
+public class RoomInventory implements Serializable {
 
-    public RoomInventory() {
-        inventory = new HashMap<>();
+    private static final long serialVersionUID = 1L;
+
+    private Map<String, Integer> inventory = new HashMap<>();
+
+    public void addRoomType(String type, int count) {
+        inventory.put(type, count);
     }
 
-    public void addRoomType(String roomType, int count) {
-        inventory.put(roomType, count);
+    public int getAvailability(String type) {
+        return inventory.getOrDefault(type, 0);
     }
 
-    public int getAvailability(String roomType) {
-        return inventory.getOrDefault(roomType, 0);
+    public synchronized boolean isAvailable(String type) {
+        return getAvailability(type) > 0;
     }
 
-    public void updateAvailability(String roomType, int newCount) {
-        if (inventory.containsKey(roomType)) {
-            inventory.put(roomType, newCount);
-        }
+    public synchronized void decrement(String type) {
+        inventory.put(type, getAvailability(type) - 1);
     }
 
     public void displayInventory() {
-        System.out.println("\nCurrent Room Inventory:");
-        for (Map.Entry<String, Integer> entry : inventory.entrySet()) {
-            System.out.println(entry.getKey() + " : " + entry.getValue() + " rooms available");
+        System.out.println("\nInventory:");
+        for (Map.Entry<String, Integer> e : inventory.entrySet()) {
+            System.out.println(e.getKey() + " -> " + e.getValue());
         }
-    }
-
-
-    public synchronized boolean isAvailable(String roomType) {
-    return getAvailability(roomType) > 0;
-}
-
-
-    public synchronized void decrement(String roomType) throws InvalidBookingException {
-    int current = getAvailability(roomType);
-
-    if (current <= 0) {
-        throw new InvalidBookingException("No rooms available for " + roomType);
-    }
-
-    updateAvailability(roomType, current - 1);
-}
     }
 }
